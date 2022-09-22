d5cf76c8b96ef5d6585f3ddbd8aaf3c725c49ff2
Dillon M <dillon32300@gmail.com>
2022-09-22 08:25:31 -0500

    saving

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
+app.get('/404', (req, res) => {
+    res.render('404')
+})
+app.listen(3000)
